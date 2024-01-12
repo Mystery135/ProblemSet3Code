73e85da164e11cadb54fa37f27113a456afb3b35
Mystery135 <websiteemail12345678910@gmail.com>
2024-01-11 17:05:48 -0800

    Initial commit

---
+++ src/Q2.java
@@ -0,0 +1,16 @@
+public class Q2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i<6; i++){
+            System.out.print("#");
+            for (int k = 6-i; k<6; k++){
+                System.out.print(" ");
+            }
+            System.out.print("#");
+            System.out.println();
+        }
+
+
+
+    }
+}
